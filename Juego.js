5434f3e4d5a876ba0fff146c90d87800af1bf687
antonellaLe <antonellaelizabeth.ledesma@estudiantes.unahur.edu.ar>
2025-01-25 12:14:30 -0300

    Animaciones

--- Juego.js
+++ Juego.js
@@ -3,53 +3,75 @@ class Juego {
         this.app = new PIXI.Application({
             width: 800,
             height: 600,
-            backgroundColor: 0x000000,  
+            backgroundColor: 0x000000,
         });
 
         document.body.appendChild(this.app.view);
+        this.agregarFondo();
 
         this.particleContainer = new PIXI.Container();
         this.app.stage.addChild(this.particleContainer);
-   
+
         this.burbujas = [];
         this.burbujasR = [];
 
         this.cronometro = new Cronometro(this.app);
         this.cronometro.iniciar();
 
-        this.contador = new Contador(this.app);
-        this.contador.crearTexto();
+        this.contador = new Contador(this.app)
 
+//Prueba 
+const texture = PIXI.Texture.from('./assets/005.png'); 
+const sprite = new PIXI.Sprite(texture);
+
+sprite.x = 50;  
+sprite.y = 60; 
+sprite.scale.set(0.05);
+sprite.anchor.set(0.5); 
+
+this.app.stage.addChild(sprite);
+//
 
         // Burbujas
         //Cambiar a que se generen x
         for (let i = 0; i < 50; i++) {
-            const x = Math.random() * 800; 
-            const y = Math.random() * 600; 
+            const x = Math.random() * 800;
+            const y = Math.random() * 600;
             const burbuja = new Burbuja(0x0000FF, x, y, this.particleContainer);
             this.burbujas.push(burbuja);
-            const burbujaR = new Burbuja( 0x8A2BE2, x, y, this.particleContainer); 
+            const burbujaR = new Burbuja(0x8A2BE2, x, y, this.particleContainer);
             this.burbujas.push(burbujaR);
         }
 
         // Actualizar en cada frame
         this.app.ticker.add((delta) => this.actualizar(delta));
+
+
     }
 
     actualizar(delta) {
         this.burbujas.forEach(burbuja => burbuja.updateBubbles(delta));
         this.cronometro.actualizar(delta);
+
     }
 
-    
+    agregarFondo() {
+        this.contenedorFondo = new PIXI.Container();
+        this.textura = PIXI.Texture.from("Assets/fondo2.jpg");
+        this.fondoSprite = new PIXI.TilingSprite(this.textura, this.app.view.width, this.app.view.height);
+        this.contenedorFondo.addChild(this.fondoSprite);
+        this.app.stage.addChild(this.contenedorFondo);
+    }
+
+
 }
 
 
 class Burbuja {
     constructor(color, x, y, container, juego, contador) {
-        
+
         this.container = container;
-        
+
         this.juego = juego;
 
         this.contador = contador;
@@ -58,19 +80,101 @@ class Burbuja {
 
         this.bubbleTexture = PIXI.Texture.from('./assets/b1.png');
 
-        this.colorMatrix = new PIXI.filters.ColorMatrixFilter();
-
         this.bubbles = [];
 
-        this.createBubble(x, y, color); 
+        this.createBubble(x, y, color);
     }
+
+        async animacion(bubble) {
+    
+            const atlasData = {
+                frames: {
+                    b1: {
+                        frame: {x: 0, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b2: {
+                        frame: {x: 128, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b3: {
+                        frame: {x: 256, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b4: {
+                        frame: {x: 384, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b5: {
+                        frame: {x: 512, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b6: {
+                        frame: {x: 640, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    },
+                    b7: {
+                        frame: {x: 768, y: 0, w: 128, h: 128},
+                        sourceSize: {w: 128, h: 128},
+                        spriteSourceSize: {x: 0, y: 0, w: 128, h: 128}
+                    }
+                },                           
+                
+                meta: {
+                    image: './assets/explosion.png',
+                    format: 'RGBA8888',
+                    size: {w: 896, h: 198},
+                    scale: 1
+                },
+                animations: {
+                    burbujaA: ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7']
+                }
+            };
+        
+            const spritesheet = new PIXI.Spritesheet(
+                PIXI.Texture.from(atlasData.meta.image),
+                atlasData
+            );
+        
+            await new Promise((resolve) => spritesheet.parse(resolve));
+        
+            const anim = new PIXI.AnimatedSprite(spritesheet.animations.burbujaA);
+        
+    
+            anim.animationSpeed = 0.1; 
+            anim.loop = false; 
+            
+            
+            const globalPosition = bubble.getGlobalPosition();
+    
+             
+            anim.x = globalPosition.x - bubble.width / 2; 
+            anim.y = globalPosition.y - bubble.height / 2;
+    
+            anim.play();
+    
+            anim.onComplete = () => {
+                this.container.removeChild(anim);
+                anim.destroy();
+            };
+            
+        
+            // Agregar la animación al stage
+            this.container.addChild(anim);
+        }
 
     createBubble(x, y, color) {
         const bubble = new PIXI.Sprite(this.bubbleTexture);
         bubble.anchor.set(0.5);
 
-        bubble.x = x || 300; 
-        bubble.y = y || 600; 
+        bubble.x = x || 300;
+        bubble.y = y || 600;
 
         // Velocidades iniciales aleatorias
         const angle = Math.random() * Math.PI * 2; // Ángulo aleatorio
@@ -83,11 +187,8 @@ class Burbuja {
 
         // Transparencia y escala aleatorias
         bubble.alpha = Math.random() * 0.5 + 0.5;
-        bubble.scale.set(Math.random() * 0.5 + 0.5);
+        bubble.scale.set(Math.random() * 0.25 + 0.5);
 
-        // Aplicar filtro de color
-        bubble.filters = [this.colorMatrix];
-        this.colorMatrix.tint(color); // Color azul0x0000FF
 
         // Agregar al contenedor
         this.container.addChild(bubble);
@@ -98,22 +199,30 @@ class Burbuja {
         bubble.buttonMode = true;
 
         bubble.on('pointerdown', () => {
-            this.eliminarBurbuja(bubble); 
+            this.eliminarBurbuja(bubble);
+            this.animacion(bubble);
         });
-        
+
     }
 
     eliminarBurbuja(bubble) {
-        this.container.removeChild(bubble); 
+        this.explosionEnCurso = true;
+        //this.animacionExplosion();
+
+        //Puntaje no funciona
+        juego.contador.actualizar();
+        //console.log('suma uno')
+
+        this.container.removeChild(bubble);
         const index = this.bubbles.indexOf(bubble); // Eliminar del array
         if (index > -1) {
             this.bubbles.splice(index, 1);
         }
 
-        //Puntaje no funciona
-        this.contador.actualizar();
-        console.log('suma uno')
+
+
     }
+
 
     updateBubbles(delta) {
         for (const bubble of this.bubbles) {
@@ -123,17 +232,17 @@ class Burbuja {
             bubble.phase += 0.05 * delta; // Actualizar fase para oscilación
 
             // Bordes laterales
-            if (bubble.x < 0 || bubble.x > 800) { 
-                bubble.vx *= -1; 
+            if (bubble.x < 0 || bubble.x > 800) {
+                bubble.vx *= -1;
             }
 
             // Bordes superior e inferior
             if (bubble.y < 0) {
-                bubble.vy *= -0.8; 
+                bubble.vy *= -0.8;
                 bubble.y = 0;
             }
-            if (bubble.y > 600) { 
-                bubble.vy *= -0.8; 
+            if (bubble.y > 600) {
+                bubble.vy *= -0.8;
                 bubble.y = 600;
             }
         }
@@ -143,7 +252,7 @@ class Burbuja {
 class Cronometro {
     constructor(app) {
         this.app = app;
-        this.tiempoTotal = 60; 
+        this.tiempoTotal = 60;
         this.texto = null;
         this.tiempoRestante = this.tiempoTotal;
 
@@ -165,7 +274,7 @@ class Cronometro {
 
     actualizar(delta) {
         if (this.tiempoRestante > 0) {
-            this.tiempoRestante -= delta / 60; 
+            this.tiempoRestante -= delta / 60;
             const minutos = Math.floor(this.tiempoRestante / 60);
             const segundos = Math.floor(this.tiempoRestante % 60);
             this.texto.text = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
@@ -175,15 +284,16 @@ class Cronometro {
     }
 
     iniciar() {
-        this.tiempoRestante = this.tiempoTotal; 
+        this.tiempoRestante = this.tiempoTotal;
     }
 }
 
 class Contador {
     constructor(app) {
-        this.app = app; 
-        this.texto = null; 
-        this.puntaje = 0; 
+        this.app = app;
+        this.texto = null;
+        this.puntaje = 0;
+        this.crearTexto();
     }
 
     crearTexto() {
@@ -194,18 +304,18 @@ class Contador {
             align: 'center',
         });
         this.texto.anchor.set(0.5);
-        this.texto.x = 100; 
-        this.texto.y = 50;  
+        this.texto.x = 120;
+        this.texto.y = 50;
         this.app.stage.addChild(this.texto);
     }
 
     actualizar() {
-        this.puntaje += 1; 
-        this.texto.text = this.puntaje.toString(); 
+        this.puntaje += 1;
+        this.texto.text = this.puntaje.toString();
     }
 }
 
-class Player{ //Puntos- mouse- etc
+class Player { //Puntos- mouse- etc
 
 }
 
